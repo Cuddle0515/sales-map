5d5a14fee3bf2ae60b1185eceef219b38a3ec094
Cuddleota <kaito_ota@cuddle-co.jp>
2026-03-16 18:21:46 +0900

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,8 @@ const CONFIG = {
   // Google スプレッドシートのCSV公開URL
   // スプレッドシート → ファイル → 共有 → ウェブに公開 → CSV形式のURLを貼り付け
   // 例: "https://docs.google.com/spreadsheets/d/XXXX/export?format=csv&gid=0"
-  SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/17Ohh1iiuFbwnCp2_GX5qQur9hU6M_8HRWk1_0k8Dlug/export?format=csv&gid=0",
+  SHEET_CSV_URL:  "https://docs.google.com/spreadsheets/d/e/2PACX-1vT529vD--xDytnL1dkzadkvXVS7
+  2ZSyEDHmEGQwHqt0FVFMr33K-RnZArPXCcGdXY_V1iss8IOny3yj/pub?output=csv",
 
   // 地図の初期表示設定
   MAP_CENTER: { lat: 36.2048, lng: 138.2529 }, // 日本中心
